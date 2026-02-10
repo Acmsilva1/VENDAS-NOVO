ce6579dcd9ec980d625b49a30a018bf63fc281f3
André Carlos de Miranda Silva <andrecarlos.miranda@gmail.com>
2026-02-10 20:39:48 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'dash-transparencia-v2';
+const ASSETS = [
+  '/',
+  'https://cdn.jsdelivr.net/npm/chart.js' // Caso você decida voltar com os gráficos
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
