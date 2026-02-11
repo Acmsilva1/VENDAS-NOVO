401eab4a06190d30b77084040267eb8e70c05bbc
André Carlos de Miranda Silva <andrecarlos.miranda@gmail.com>
2026-02-10 22:55:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dash-transparencia-v2';
+const CACHE_NAME = 'dash-transparencia-v3';
 const ASSETS = [
   '/',
   'https://cdn.jsdelivr.net/npm/chart.js' // Caso você decida voltar com os gráficos
